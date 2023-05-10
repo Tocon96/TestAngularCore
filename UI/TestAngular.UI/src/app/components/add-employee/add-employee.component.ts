ca9486223746461f5d5c88ac4aa26965bd4bd817
Tocon96 <bultocon@gmail.com>
2023-05-10 14:44:29 +0200

    Adding UI

---
+++ UI/TestAngular.UI/src/app/components/add-employee/add-employee.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../../models/employee.model';
+import { EmployeesService } from '../../services/employees.service';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent {
+  departments: string[] = [
+    'HR',
+    'IT',
+    'CEO'
+  ]
+
+  addEmployeeRequest: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: ''
+  };
+
+  constructor(private employeesService: EmployeesService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addEmployee() {
+    this.employeesService.addEmployee(this.addEmployeeRequest)
+      .subscribe({
+        next: (employee) => {
+          this.router.navigate(['employees']);
+        },
+        error: (response) => {
+          console.log(response);
+        }
+      });
+  }
+
+}
---
+++ UI/TestAngular.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Employee } from '../../../models/employee.model';
+import { EmployeesService } from '../../../services/employees.service';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
+})
+export class EmployeesListComponent {
+  employees: Employee[] = [];
+
+  constructor(private employeesService: EmployeesService) { }
+
+  ngOnInit(): void {
+    this.employeesService.getAllEmployees()
+      .subscribe({
+        next: (employees) => {
+          this.employees = employees;
+        },
+        error: (response) => {
+          console.log(response);
+        }
+      });
+  }
+}
